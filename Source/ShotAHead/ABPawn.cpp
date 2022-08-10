1c0210a909a84736b9e2982678ad6378436e83c6
MomDra <yem9087@naver.com>
2022-08-10 23:42:12 +0900

    Move player

--- Source/ShotAHead/ABPawn.cpp
+++ Source/ShotAHead/ABPawn.cpp
@@ -9,6 +9,30 @@ AABPawn::AABPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CAPSULE"));
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MESH"));
+	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MOVEMENT"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+
+	RootComponent = Capsule;
+	Mesh->SetupAttachment(Capsule);
+	SpringArm->SetupAttachment(Capsule);
+	Camera->SetupAttachment(SpringArm);
+
+	Capsule->SetCapsuleHalfHeight(88.0f); // 캐릭터의 절반 높이 어떻게 구하지?
+	Capsule->SetCapsuleRadius(34.0f); // 캐릭터의 몸둘레
+
+	// 언리얼 액터 기준 위치: 정중앙, 에디터: 발바닥 위, 회전으로 애셋 애디터 상(블랜더..)의 좌표축과 언리얼의 좌표축을 맞추는 작업임
+	Mesh->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -88.0f), FRotator(0.0f, -90.0f, 0.0f)); 
+	SpringArm->TargetArmLength = 400.0f;
+	SpringArm->SetRelativeRotation(FRotator(-15.0f, 0.0f, -0.0f));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> WIZARD(TEXT("SkeletalMesh'/Game/BattleWizardPolyart/Meshes/WizardSM.WizardSM'"));
+	if (WIZARD.Succeeded()) {
+		Mesh->SetSkeletalMesh(WIZARD.Object);
+	}
 }
 
 // Called when the game starts or when spawned
@@ -42,5 +66,19 @@ void AABPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AABPawn::UpDown);
+	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AABPawn::LeftRight);
+}
+
+void AABPawn::UpDown(float NewAxisValue)
+{
+	//ABLOG(Warning, TEXT("%f"), NewAxisValue);
+	AddMovementInput(GetActorForwardVector(), NewAxisValue);
+}
+
+void AABPawn::LeftRight(float NewAxisValue)
+{
+	//ABLOG(Warning, TEXT("%f"), NewAxisValue);
+	AddMovementInput(GetActorRightVector(), NewAxisValue);
 }
 
