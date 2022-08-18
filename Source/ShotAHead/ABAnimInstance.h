5020de0f3065d277b5fdbcf375e79d890093eaf9
MomDra <yem9087@naver.com>
2022-08-18 14:15:42 +0900

    Animation idle walk

---
+++ Source/ShotAHead/ABAnimInstance.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ShotAHead.h"
+#include "Animation/AnimInstance.h"
+#include "ABAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOTAHEAD_API UABAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UABAnimInstance();
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Pawn, Meta = (AllowPrivateAccess = true))
+		float CurrentPawnSpeed;
+};
